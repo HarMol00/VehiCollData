1fa55e7638ce4ed3c4d152ab6b6d1cfcd8fbf192
Harald Möller <harald.moeller@omicronenergy.com>
2025-03-16 11:43:13 +0100

    updated by VehiColl on HARMOL00LW02.omicron.at

---
+++ 1280.06.js
@@ -0,0 +1,16 @@
+var prgList = [
+ '1|3||Basis-Adresse (kurze Adresse)',
+ '2|3||Minimale Geschwindigkeit',
+ '3|11||Faktor Beschleunigung (Anfahr-Verzögerung)',
+ '4|3||Faktor Bremsen (Brems-Verzögerung)',
+ '5|100||Maximale Geschwindigkeit',
+ '6|50||Mittlere Geschwindigkeit',
+ '7|255||Decoder Versionsnummer',
+ '8|151||Hersteller-ID',
+ '9|40||Periode Pulsweite',
+ '17|197||Erweiterte Adresse (lange Adresse), High Byte',
+ '18|0||Erweiterte Adresse (lange Adresse), Low Byte',
+ '28|131||Konfiguration RailCom',
+ '29|46||Decoder-Konfiguration',
+ '124|4||Erweiterte Konfiguration #2',
+];
